f00aa378051684ae7723f07d5144098ffeefe19b
tfia <duruohe@outlook.com>
2025-01-15 23:48:21 +0800

    phase 4

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,45 @@
 mod bfir;
 
+use dynasm::dynasm;
+use dynasmrt::{DynasmApi, DynasmLabelApi};
+
+use std::io::{stdout, Write};
+
+unsafe extern "sysv64" fn print(buf: *const u8, len: u64) -> u8 {
+    let ret = std::panic::catch_unwind(|| {
+        let buf = std::slice::from_raw_parts(buf, len as usize);
+        stdout().write_all(buf).is_err()
+    });
+    match ret {
+        Ok(false) => 0,
+        Ok(true) => 1,
+        Err(_) => 2
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut ops = dynasmrt::x64::Assembler::new().unwrap();
+    let hello_string = b"Hello, JIT!\n";
+
+    dynasm!(ops
+        ; .arch x64
+        ; ->hello :
+        ; .bytes hello_string);
+
+    let hello = ops.offset();
+    dynasm!(ops
+        ; lea rdi, [->hello]
+        ; mov rsi, QWORD hello_string.len() as _
+        ; mov rax, QWORD print as _
+        ; call rax
+        ; ret);
+
+    let buf = ops.finalize().unwrap();
+
+    let hello_fn: unsafe extern "sysv64" fn() -> u8 = 
+        unsafe { std::mem::transmute(buf.ptr(hello)) };
+
+    let ret = unsafe { hello_fn() };
+
+    assert_eq!(ret, 0);
 }
